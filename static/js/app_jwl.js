d0e841e0e269369c9a37478638b067823628e8df
jwhlaforest <jwh.laforest@gmail.com>
2018-10-23 20:42:26 -0500

    some progress

--- static/js/app_jwl.js
+++ static/js/app_jwl.js
@@ -13,5 +13,54 @@ function buildMetadata(sample) {
 };
 
 function buildCharts(sample) {
+  var url = `/samples/$(sample)`;
+  d3.json(url).then(function(data) {
+    var xValues = data.bball_id;
+    var yValues = data.sample_values;
+    var tValues = data.bball_label;
+    var mSize = data.sample_values;
+    var mClrs = data.bball_id;
+
+    var trace1 = {
+      x: xValues,
+      y: yValues,
+      text: tValues,
+      mode: 'markers',
+      marker: {
+        size: mSize,
+        color: mClrs
+      }
+    };
+
+    var data = [trace1];
+
+    var layout = {
+      xaxis: {title: "Baseball Team"}
+    };
   
-}
+    Plotly.newPlot(`bubble`, data, layout);
+  });
+};
+
+function init() {
+  var selector = d3.select("#selDataset");
+  d3.json("/teams").then((sampleTeams) => {
+    sampleTeams.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    const firstSample = sampleTeams[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+};
+
+function optionChanged(newSample) {
+  buildCharts(newSample);
+  buildMetadata(newSample);
+};
+
+init();
